72553617b4ce08ad33a6e248019533190edbce70
mahali-asmah <79982455+mahali-asmah@users.noreply.github.com>
2023-12-19 14:29:10 +0000

    test: add test and github workflow (#19)
    * test: add test and github workflow
    
    * fix: adjust to pr comments

---
+++ src/test/tests-utils.tsx
@@ -0,0 +1,46 @@
+import {
+  queries,
+  render as rtlRender,
+  type RenderOptions,
+  type RenderResult,
+} from '@testing-library/react';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { vi } from 'vitest';
+
+import { Providers } from './providers';
+
+type UI = JSX.Element;
+
+export type Options = {
+  router?: Partial<AppRouterInstance>;
+} & RenderOptions;
+
+const mockedRouter: AppRouterInstance = {
+  back: vi.fn(),
+  forward: vi.fn(),
+  push: vi.fn(),
+  replace: vi.fn(),
+  refresh: vi.fn(),
+  prefetch: vi.fn(),
+};
+
+const customRender = (
+  ui: UI,
+  { router, ...options }: Options = {},
+): RenderResult => {
+  const Wrapper = ({ children }: { children: JSX.Element }) => {
+    return (
+      <Providers router={{ ...mockedRouter, ...(router ? { ...router } : {}) }}>
+        {children}
+      </Providers>
+    );
+  };
+
+  return rtlRender(ui, {
+    wrapper: Wrapper,
+    queries,
+    ...options,
+  });
+};
+
+export { customRender as render };
