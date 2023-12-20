dbbad0bdbf6570f5d83a47f7b37f6ed79c406870
Mahali <mahali.asmah@earlynode.com>
2023-12-20 07:10:48 +0000

    test: add support for msw & playwright

---
+++ playwright/e2e/landing/landing.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Landing', () => {
+  test('displays a landing page', async ({ page }) => {
+    const url = `/`;
+    await page.goto(url);
+
+    expect(page.url()).toEqual('http://localhost:3000/');
+
+    await expect(
+      page.getByRole('heading', { level: 1, name: /next js starter/i }),
+    ).toBeVisible();
+
+    await expect(
+      page.getByRole('link', { name: /earlynode/i }),
+    ).toHaveAttribute('href', 'https://earlynode.com');
+
+    await expect(page.getByRole('img')).toBeVisible();
+
+    await page.close();
+  });
+});
